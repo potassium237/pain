da7b1e6e30fd392737dc9a1bd326b52bc6e0d2af
potassium237 <yuhaann@gmail.com>
2023-12-04 11:19:45 +0800

    square move

--- tba/Assets/test_movement.cs
+++ tba/Assets/test_movement.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class test_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 speed = new Vector2(50,50);
 
     // Update is called once per frame
     void Update()
     {
-        
+        float inputY = Input.GetAxis("Vertical");
+        float inputX = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(speed.x*inputX, speed.y*inputY, 0);
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
     }
 }
