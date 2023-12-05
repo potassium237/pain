0c9539a331178235857242701e1a1599d58e6206
potassium237 <yuhaann@gmail.com>
2023-12-05 23:28:14 +0800

    box move part 2 cause i deleted part 1 heh

---
+++ tba/Assets/player_movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {float inputX = Input.GetAxis("Horizontal");
+     float inputY = Input.GetAxis("Vertical");
+
+     Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+     movement *= Time.deltaTime;
+
+     transform.Translate(movement);
+        
+    }
+}
