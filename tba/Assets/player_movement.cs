bd57c2cbbeba209ac6b5a1c2839a9301758a2b51
potassium237 <yuhaann@gmail.com>
2023-12-07 17:59:28 +0800

    object flip when ove

--- tba/Assets/player_movement.cs
+++ tba/Assets/player_movement.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class player_movement : MonoBehaviour
 {
     public float speed = 10f;
+    SpriteRenderer spi;
     // Start is called before the first frame update
     void Start()
-    {}
+    {
+        spi = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,6 +18,12 @@ public class player_movement : MonoBehaviour
      float inputY = Input.GetAxis("Vertical");
      
      Vector3 movement = new Vector3(speed * inputX, speed * inputY, 0);
+
+     if (inputX == -1 && spi.flipX == false){
+            spi.flipX = true;
+        }else if (inputX == 1 && spi.flipX == true){
+            spi.flipX = false;
+        }
 
      movement *= Time.deltaTime;
 
